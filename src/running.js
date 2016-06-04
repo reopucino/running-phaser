23dbef3d856083562ba7f61b3a5fac2cc4d7c8ba
Reza Rezqya Akbar <reza.rezqya@gmail.com>
2016-06-04 19:39:28 +0700

    adding player anim and make the move

--- src/running.js
+++ src/running.js
@@ -11,10 +11,17 @@ var playgame = function(game){};
 
 playgame.prototype = {
 	p1RunCon:false,
+	p2RunCon:false,
+	framePlayerOne : [1,9,10],
+	framePlayerTwo : [12,20,21],
+
+	p1CurrentFrame : 0,
+	p2CurrentFrame : 0,
+
 
 	preload:function(){
 		game.load.spritesheet("player", "./assets/spritesheet_player.png", 23,23, 22);
-		game.load.spritesheet("player2", "./assets/spritesheet_player.png", 23,23, 22);
+		//game.load.spritesheet("player2", "./assets/spritesheet_player.png", 23,23, 22);
 		game.scale.pageAlignHorizontally = true;
 		game.scale.oageAlignVertically = true;
 		game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -22,15 +29,27 @@ playgame.prototype = {
 	
 	create:function(){
 		game.stage.backgroundColor = "#000000";
-		this.player = game.add.sprite(30, 100, "player");
-		this.playerTwo = game.add.sprite(30, 50, "player2");
-		this.playerTwo.animations.add('run');
-		this.playerTwo.animations.play("run", 30, true);
+		this.playerOne = game.add.sprite(30, 100, "player");
+		this.playerTwo = game.add.sprite(30, 50, "player");
+		//this.playerOne.animations.add('run', [1,9,10]);
+		//this.playerOne.animations.add('idle', [0]);
+		//this.playerOne.animations.play('run', 20, true);
+		//console.log(this.playerOne.animations.currentAnim);
+		//this.playerOne.animations.currentAnim=;
+
+		//this.playerTwo.animations.add('run', [12,20,21]);
+		//this.playerTwo.animations.add('idle', [11]);
+		this.playerTwo.animations.frame = 11;
+		//this.playerTwo.animations.play('run', 20, true);
 		var tickRpOne = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
 		tickRpOne.onDown.add(this.rightMovePlayerOne, this);
 		var tickLpOne = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-		tickLpOne.onDown.add(this.leftMovePlayerTwo, this);
-		//this.player2 = game.add.sprite
+		tickLpOne.onDown.add(this.leftMovePlayerOne, this);
+
+		var tickLpTwo = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+		tickLpTwo.onDown.add(this.rightMovePlayerTwo, this);
+		var tickRpTwo = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+		tickRpTwo.onDown.add(this.leftMovePlayerTwo, this);
 	},
 	
 	update:function(){
@@ -40,14 +59,42 @@ playgame.prototype = {
 	rightMovePlayerOne:function(){
 		if(!this.p1RunCon){
 			this.p1RunCon = true;
-			this.player.x += 1;
+			this.playerOne.x += 1;
+			this.p1CurrentFrame++;
+			if(this.p1CurrentFrame > 2)this.p1CurrentFrame = 0;
+			this.playerOne.animations.frame = this.framePlayerOne[this.p1CurrentFrame];
+			//this.playerOne.animations.frame = 1;
+		}
+	},
+
+	leftMovePlayerOne:function(){
+		if(this.p1RunCon){
+			this.p1RunCon = false;
+			this.playerOne.x += 1;
+			this.p1CurrentFrame++;
+			if(this.p1CurrentFrame > 2)this.p1CurrentFrame = 0;
+			this.playerOne.animations.frame = this.framePlayerOne[this.p1CurrentFrame];
+			//this.playerOne.animations.frame = 9;
+		}
+	},
+
+	rightMovePlayerTwo:function(){
+		if(!this.p2RunCon){
+			this.p2RunCon = true;
+			this.playerTwo.x += 1;
+			this.p2CurrentFrame++;
+			if(this.p22urrentFrame > 2)this.p1CurrentFrame = 0;
+			this.playerTwo.animations.frame = this.framePlayerTwo[this.p2CurrentFrame];
 		}
 	},
 
 	leftMovePlayerTwo:function(){
-		if(this.p1RunCon){
-			this.p1RunCon = false;
-			this.player.x += 1;
+		if(this.p2RunCon){
+			this.p2RunCon = false;
+			this.playerTwo.x += 1;
+			this.p2CurrentFrame++;
+			if(this.p2CurrentFrame > 2)this.p2CurrentFrame = 0;
+			this.playerTwo.animations.frame = this.framePlayerTwo[this.p2CurrentFrame];
 		}
 	}
 }
